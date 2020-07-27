71de05580be1315f0beebdb31255fc198eced723
boosterh <boosterh@mix.wvu.edu>
2020-07-27 08:33:58 -0600

    Update test_r_ci.R

--- tests/testthat/test_r_ci.R
+++ tests/testthat/test_r_ci.R
@@ -2,7 +2,7 @@ library(apaTables)
 context("r calculation")
 test_that("Test r calculation and CI", {
      expect_equal(cor_test1()$r,".83**")
-     expect_equal(cor_test1()$CI,"[.66, .91]")
+     expect_equal(cor_test1()$CI,"1")
      expect_equal(cor_test1()$m,"64.63")
      expect_equal(cor_test1()$sd,"12.17")
 })
